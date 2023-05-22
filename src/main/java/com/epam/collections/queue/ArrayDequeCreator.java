f490042b17cad69536940095969b88fb7a9d5c06
VladimirKameko <drugs1360@gmail.com>
2023-05-22 19:01:33 +0300

    First commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,36 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> res = new ArrayDeque<>();
+
+        res.add(firstQueue.peek());
+        firstQueue.remove();
+        res.add(firstQueue.peek());
+        firstQueue.remove();
+
+        res.add(secondQueue.peek());
+        secondQueue.remove();
+        res.add(secondQueue.peek());
+        secondQueue.remove();
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            firstQueue.add(res.getLast());
+            res.removeLast();
+
+            res.add(firstQueue.peek());
+            firstQueue.remove(firstQueue.peek());
+            res.add(firstQueue.peek());
+            firstQueue.remove(firstQueue.peek());
+
+            secondQueue.add(res.getLast());
+            res.removeLast();
+
+            res.add(secondQueue.peek());
+            secondQueue.remove(secondQueue.peek());
+            res.add(secondQueue.peek());
+            secondQueue.remove(secondQueue.peek());
+        }
+        return res;
+
+
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DishOrderDeterminer {
+
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        int step = everyDishNumberToEat - 1;
+        int index = 0;
+        List<Integer> res = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            list.add(i);
+        }
+
+        while (!list.isEmpty()) {
+
+            index = (index + step) % list.size();
+            res.add(list.remove(index));
+
+        }
+        return res;
     }
 }
